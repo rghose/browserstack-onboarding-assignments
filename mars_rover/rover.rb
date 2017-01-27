cdb9524ffbd7c0366d82814b7e73786a8e6534c7
Rahul Ghose <rahul.g@browserstack.com>
2017-01-27 17:35:35 +0530

    added map checks. improve test case for boundary check.

--- mars_rover/rover.rb
+++ mars_rover/rover.rb
@@ -4,6 +4,11 @@
 # By Rahul Ghose
 # 2017-01-25
 # https://gist.github.com/AnkurGel/d4eb058e1f4fd5c76255671327db70a4
+#
+# Assumptions:
+# 1. rovers *cannot* cross boundaries
+# 2. rovers can occupy a position already occupied by other rover(s)
+#
 
 # directions in anti-clockwise order
 DIRECTIONS = [ "E", "N", "W", "S" ]
@@ -17,8 +22,24 @@ LR = [ "L", "R" ]
 
 DEBUG = false
 
+class Map
+  def initialize(x=0,y=0)
+    @width = Integer(x)
+    @height = Integer(y)
+  end
+
+  def height
+    @height
+  end
+
+  def width
+    @width
+  end
+end
+
 class Rover
   private
+  @map
   @position
   @orientation
 
@@ -31,9 +52,10 @@ class Rover
   end
 
   public
-  def initialize(x=0, y=0, o="N")
+  def initialize(x=0, y=0, o="N", width=100, height=100)
     set_position(x,y)
     set_orientation(o)
+    @map = Map.new(width, height)
   end
 
   def show()
@@ -50,15 +72,16 @@ class Rover
     end
   end
 
+  # Move the rover, keeping the borders in mind
   def move_rover()
     puts  "DEBUG: moving: #{@orientation}" if DEBUG
     case @orientation
       when DIRECTION_EAST
-        @position[:x] += 1
+        @position[:x] += 1 if @position[:x] < @map.width
       when DIRECTION_WEST
         @position[:x] -= 1 if @position[:x] > 0
       when DIRECTION_NORTH
-        @position[:y] += 1
+        @position[:y] += 1 if @position[:y] < @map.height
       when DIRECTION_SOUTH
         @position[:y] -= 1 if @position[:y] > 0
     end
@@ -87,7 +110,7 @@ def main()
       break
     end
     x,y,o = input_array
-    r = Rover.new(Integer(x),Integer(y),o)
+    r = Rover.new(Integer(x),Integer(y),o, max_x, max_y)
     moves = gets.chomp
     moves.each_char do |move|
       r.run(move)
@@ -95,7 +118,6 @@ def main()
     puts "DEBUG: #{r.show()}" if DEBUG
     rovers.push(r)
   end
-  puts rovers.length
   rovers.each { |r|
     puts r.show()
   }
